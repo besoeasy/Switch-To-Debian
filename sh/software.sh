9f05a3b31564a18c3f35b01d82a0a5ca0442d0b2
besoeasy <besoeasy@users.noreply.github.com>
2023-12-31 02:31:39 +0530

    Update software.sh script to install git and corresponding Flatpak/Snapd backends for GNOME and KDE desktop environments

--- sh/software.sh
+++ sh/software.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
 DESKTOP_ENV=""
-# Detect desktop environment
+
 if [ -n "$XDG_CURRENT_DESKTOP" ]; then
     DESKTOP_ENV=$(echo "$XDG_CURRENT_DESKTOP" | tr '[:upper:]' '[:lower:]')
 fi
 
-# Install Flatpak and Snapd
 sudo apt update -y
-sudo apt install flatpak snapd -y
-sudo apt install nala wget curl vlc -y
 
-# Install corresponding Flatpak backend for the detected desktop environment
+sudo apt install flatpak snapd -y
+sudo apt install nala wget curl git -y
+
 if [ "$DESKTOP_ENV" = "gnome" ]; then
     sudo apt install gnome-software-plugin-flatpak -y
+    sudo apt install gnome-software-plugin-snap -y
 elif [ "$DESKTOP_ENV" = "kde" ]; then
     sudo apt install plasma-discover-backend-flatpak -y
+    sudo apt install plasma-discover-backend-snap -y
 else
     echo "Desktop environment not recognized or not supported."
     exit 1
 fi
 
-# Add Flathub repository
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-# Add Snap Store repository
 sudo snap install core
